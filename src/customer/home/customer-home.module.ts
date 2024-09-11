49a60fc5d2625494fe94bc18640511f3435f7ff6
Thịnh Văn <thinhpk26@gmail.com>
2024-09-12 00:24:13 +0700

    feat: complete header

--- src/customer/home/customer-home.module.ts
+++ src/customer/home/customer-home.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { CustomerHomeController } from './customer-home.controller';
 import { productProviders } from 'src/share/providers/product.provider';
-import { DatabaseModule } from 'src/share/providers/database.module';
 import { ProductService } from 'src/share/services/product.service';
 
 @Module({
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,32 @@
 module.exports = {
   content: ['./views/**/*.{html,hbs}', './public/**/*.{css,js}'],
   theme: {
+    colors: {
+      gray: '#f5f5f5',
+      transparent: 'transparent',
+      'main-blue': '#004aad',
+      'main-yellow': '#ffbd59',
+      black: '#232323',
+      "black-light": '#444444',
+      "b-separate": "#d0d0d0"
+    },
+    fontFamily: {
+      base: ["Montserrat"], // font chính
+      lato: ["Lato"] // font cho những thông tin thêm
+    },
+    fontWeight: {
+      light: "300",
+      regular: "400",
+      medium: "500",
+      bold: "700"
+    },
+    fontSize: {
+      '4': ['16px', '24px'],
+      '3': ['12px', '16px'],
+      '3.5': ['14px', '20px'],
+      '4.5': ['18px', '28px'],
+      '5': ['20px', '28px']
+    },
     extend: {},
   },
   plugins: [],
